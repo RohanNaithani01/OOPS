1ad38e8dae49cc6ba6f6bf3bb9546bc4290701ee
RohanNaithani01 <rohannaithani02@gmail.com>
2026-05-12 08:54:39 +0530

    OOPS InnerClass

---
+++ src/main/java/org/example/InnerClass.java
@@ -0,0 +1,18 @@
+package org.example;
+
+class outerClass{
+    static int val=10;
+
+    class InnerClass{
+        public void execute(){
+            System.out.println("Inner Class executed "+ val);
+        }
+    }
+
+}
+public class InnerClass {
+    public static void main(String[] args) {
+        outerClass.InnerClass innerClass=new outerClass().new InnerClass();
+        innerClass.execute();
+    }
+}
